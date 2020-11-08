f2a3c9221b4500098217c6f1e677864e24996300
iyashjayesh <180303105046@paruluniversity.ac.in>
2020-11-08 15:16:55 +0530

    Version 0.1
    Project Source Code

---
+++ Assets/GazeVR.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class GazeVR : MonoBehaviour
+{
+
+    public Image imgCircle;
+    public UnityEvent GVRClick;
+    public float totalTime = 2;
+    bool gvrStatus;
+    public float gvrTimer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gvrStatus){
+            gvrTimer += Time.deltaTime;
+            imgCircle.fillAmount = gvrTimer / totalTime;
+        }
+
+        if(gvrTimer > totalTime){
+            GVRClick.Invoke();
+        }
+    }
+
+    public void GvrOn(){
+        gvrStatus = true;
+    }
+    
+    public void GvrOff(){
+        gvrStatus = false;
+        gvrTimer = 0;
+        imgCircle.fillAmount = 0;
+    }
+}
---
+++ Assets/Scenes/csnBtn.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class csnBtn : MonoBehaviour
+{
+    public void changeScene(string sceneName){
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitApp(){
+        Application.Quit();
+    }
+}
